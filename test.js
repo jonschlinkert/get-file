fec790da7776206dff77c9d1818013bc958bebad
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-31 16:17:28 -0400

    run update

--- test.js
+++ test.js
@@ -1,53 +1,62 @@
 /*!
  * get-file <https://github.com/jonschlinkert/get-file>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-/* deps:mocha */
+require('mocha');
 var assert = require('assert');
-var client = require('./');
+var get = require('./');
 
-describe('get-file', function () {
-  it('should list files from a repository', function (done) {
-    client.listFiles('jonschlinkert/get-file', function (err, files) {
-      if (err) return done(err);
+describe('get-file', function() {
+  it('should list files from a repository', function(cb) {
+    get.files('jonschlinkert/get-file', function(err, files) {
+      if (err) {
+        cb(err);
+        return;
+      }
+
       assert(Array.isArray(files));
-      assert(files.length)
-      done();
+      assert(files.length);
+      cb();
     });
   });
 
-  it('should download a file from a repository', function (done) {
-    client.getFile('jonschlinkert/get-file', 'package.json', function (err, res) {
-      if (err) return done(err);
+  it('should download a file from a repository', function(cb) {
+    get('jonschlinkert/get-file', 'package.json', function(err, res) {
+      if (err) {
+        cb(err);
+        return;
+      }
+
       var contents = '';
-      res.on('data', function (data) {
+      res.on('data', function(data) {
         contents += data.toString();
       })
-      .on('error', done)
-      .on('end', function () {
+      .on('error', cb)
+      .on('end', function() {
         try {
           var pkg = JSON.parse(contents);
           assert(pkg.name, 'get-file');
-          done();
+          cb();
         } catch (err) {
-          done(err);
+          cb(err);
         }
       });
     });
   });
 
-  it('should return an error when file does not exist in repository', function (done) {
-    client.getFile('jonschlinkert/get-file', 'foo-bar-baz.badfile', function (err, res) {
+  it('should return an error when file does not exist in repository', function(cb) {
+    get('jonschlinkert/get-file', 'foo-bar-baz.badfile', function(err, res) {
       if (err) {
-        assert(typeof res === 'undefined');
-        return done();
+        assert(res == null);
+        cb();
+        return;
       }
-      done(new Error('Execpted a not found error'));
+      cb(new Error('expected a "not found" error'));
     });
   });
 });
